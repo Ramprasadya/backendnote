9cd46af785a9bffcfb75e06dff22ac557d39dffc
Ramprasadya <yadavramprasad563@gmail.com>
2023-05-06 14:42:48 +0530

    created auth and notes routes

---
+++ backend/models/Notes.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const notesSchema = new mongoose.Schema({
+    title:{
+        type : String,
+        required : true
+    },
+    description:{
+        type : String,
+        required : true,
+        
+    },
+   tag:{
+        type : String,
+       
+    },
+    date:{
+        type :Date,
+        default : Date.now
+    }
+  });
+
+  const notes = mongoose.model('notes', notesSchema);
+
+  module.exports = notes ;
